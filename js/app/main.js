4008534cf996636338852e806640d0f6fc68e5fb
Edgar-7737 <landerextremo121@gmail.com>
2024-10-15 20:40:12 -0400

    Delete js directory

--- js/app/main.js
+++
@@ -1,5 +0,0 @@
-const btnTooggle = document.querySelector(".toggle-btn");
-
-btnTooggle.addEventListener("click", function () {
-  document.getElementById("sidebar").classList.toggle("active");
-});
